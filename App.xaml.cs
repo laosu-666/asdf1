dca19302f7923bf05fd519f17ac8624a649347b7
Your Name <you@example.com>
2023-10-12 17:23:07 +0800

    2222222222222222

---
+++ App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace znsdWPF
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+            base.OnStartup(e);
+            Console.WriteLine("1.OnStartup被触发");
+        }
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            Console.WriteLine("2.OnActivated被触发");
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            Console.WriteLine("3.OnDeactivated被触发");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            Console.WriteLine("4.OnExit被触发");
+        }
+
+        
+    }
+}
